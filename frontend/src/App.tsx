a21f1e6d9556bb2c9b8d1c3ace869db8ac5aa3a7
Satyakumarch <satyakumarchaudhary603@gmail.com>
2025-02-02 15:17:42 +0530

    frontend code is started

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <button className="bg-red-900">
+        Join room with react 
+      </button>
+    
+    </>
+  )
+}
+
+export default App
